13e0d76431fac4616a8991a11e925a05b7ad890f
hongducdev <contact.hongduc@gmail.com>
2025-11-07 14:30:35 +0700

    style(woocommerce): update styles for cart and checkout components
    - Enhance layout and responsiveness of cart and checkout forms, ensuring better usability on various screen sizes.
    - Update background colors and border styles to use secondary color for improved visual consistency.
    - Refactor input fields and buttons with updated dimensions, border radius, and font styles for a modern look.
    - Implement media queries for better adaptability in cart and checkout layouts.
    - Adjust image handling in templates to support new settings and improve overall presentation.

--- elements/templates/pxl_image/layout-1.php
+++ elements/templates/pxl_image/layout-1.php
@@ -13,67 +13,91 @@ if ( ! empty( $settings['image_link']['url'] ) ) {
 $html_id = pxl_get_element_id($settings); 
 if($settings['img_effect'] == 'pxl-image-parallax') { wp_enqueue_script( 'pxl-parallax-move-mouse'); }
 ?>
-<div id="<?php echo esc_attr($html_id) ?>" class="pxl-image-single  <?php if($settings['hide_parallax_sm'] == 'true') { echo 'pxl-disable-parallax-sm'; } ?> <?php if($settings['img_display'] == 'true') { echo 'pxl-hide-sr-lg'; } ?> <?php if(!empty($settings['img_effect'])) { echo esc_attr($settings['img_effect']); } ?> <?php echo esc_attr($settings['pxl_animate']); ?>" data-wow-delay="<?php echo esc_attr($settings['pxl_animate_delay']); ?>ms" <?php if($settings['img_effect'] == 'pxl-image-tilt') : ?>data-maxtilt="<?php echo esc_attr($settings['max_tilt']); ?>" data-speedtilt="<?php echo esc_attr($settings['speed_tilt']); ?>" data-perspectivetilt="<?php echo esc_attr($settings['perspective_tilt']); ?>"<?php endif; ?> <?php if($settings['img_effect'] == 'pxl-parallax-scroll') : ?>data-parallax='{"<?php echo esc_attr($settings['parallax_scroll_type']); ?>":<?php echo esc_attr($settings['parallax_scroll_value_x']); ?>}'<?php endif; ?>>
-    <div class="pxl-item--inner <?php if($settings['img_effect'] == 'pxl-image-border') : ?>wow<?php endif;?>" data-wow-delay="120ms">
-        <?php if($settings['source_type'] == 's_img' && !empty($settings['image']['id']) || $settings['source_type'] == 'f_img' && has_post_thumbnail()) : 
-        $image_size = !empty($settings['img_size']) ? $settings['img_size'] : 'full';
-        if(!empty($settings['image']['id'])) : $img_id = $settings['image']['id']; endif;
-        if ($settings['source_type'] == 'f_img' && has_post_thumbnail()) {
-            $img_id = get_post_thumbnail_id(get_the_ID());
-        }
-        $img  = pxl_get_image_by_size( array(
-            'attach_id'  => $img_id,
-            'thumb_size' => $image_size,
-            'class' => 'no-lazyload'
-        ) );
-        $thumbnail    = $img['thumbnail'];
-        $thumbnail_url    = $img['url']; ?>
-
-        <?php switch ($settings['image_type']) {
-            case 'bg': ?>
-            <div class="pxl-item--bg bg-image " style="background-image: url(<?php echo esc_url($thumbnail_url); ?>);">
+<div id="<?php echo esc_attr($html_id) ?>" class="pxl-image-single <?php if($settings['hide_parallax_sm'] !== 'false') { echo 'pxl-disable-parallax-sm'; } ?> <?php if($settings['img_display'] !== 'false') { echo 'pxl-hide-sr-lg'; } ?> <?php if(!empty($settings['img_effect'])) { echo esc_attr($settings['img_effect']); } ?> <?php echo esc_attr($settings['pxl_animate']); ?>" data-wow-delay="<?php echo esc_attr($settings['pxl_animate_delay']); ?>ms" <?php if($settings['img_effect'] == 'pxl-image-tilt') : ?>data-maxtilt="<?php echo esc_attr($settings['max_tilt']); ?>" data-speedtilt="<?php echo esc_attr($settings['speed_tilt']); ?>" data-perspectivetilt="<?php echo esc_attr($settings['perspective_tilt']); ?>"<?php endif; ?> <?php if($settings['img_effect'] == 'pxl-parallax-scroll') : ?>data-parallax='{"<?php echo esc_attr($settings['parallax_scroll_type']); ?>":<?php echo esc_attr($settings['parallax_scroll_value_x']); ?>}'<?php endif; ?>>
+    <div class="pxl-item--inner" data-wow-delay="120ms">
+        <?php if(!empty($settings['img_label'])) : ?>
+            <div class="pxl-item--label">
+                <?php echo esc_attr($settings['img_label']); ?>
             </div>
-            <?php break;
-
-            default: ?>
-            <div class="pxl-item--image " data-parallax-value="<?php echo esc_attr($settings['parallax_value']); ?>">
-                <?php if ( ! empty( $settings['image_link']['url'] ) ) { ?><a <?php pxl_print_html($widget->get_render_attribute_string( 'image_link' )); ?>><?php } ?>
-                <?php if ( ! empty( $img_id ) ) { echo wp_kses_post($thumbnail); } ?>
-                <?php if ( ! empty( $settings['image_link']['url'] ) ) { ?></a><?php } ?>
-            </div>
-            <?php break;
-        } ?>
-    <?php endif; ?>
-
-    <?php if(is_singular('service') && $settings['source_type'] == 'f_img' && has_post_thumbnail()) : 
-    $service_icon_type = get_post_meta(get_the_ID(), 'service_icon_type', true);
-    $service_icon_font = get_post_meta(get_the_ID(), 'service_icon_font', true);
-    $service_icon_img = get_post_meta(get_the_ID(), 'service_icon_img', true); ?>
-    <?php if($service_icon_type == 'icon' && !empty($service_icon_font)) : ?>
-        <div class="pxl-service--icon">
-            <i class="<?php echo esc_attr($service_icon_font); ?>"></i>
-        </div>
-    <?php endif; ?>
-    <?php if($service_icon_type == 'image' && !empty($service_icon_img)) : 
-        $icon_img = pxl_get_image_by_size( array(
-            'attach_id'  => $service_icon_img['id'],
-            'thumb_size' => 'full',
-        ));
-        $icon_thumbnail = $icon_img['thumbnail'];
+        <?php endif; ?>
+        <?php 
+            $current_post_id = null;
+            if (function_exists('is_shop') && is_shop()) {
+                $current_post_id = wc_get_page_id('shop');
+            } elseif (function_exists('is_product') && is_product()) {
+                $current_post_id = get_the_ID();
+            } elseif (is_singular()) {
+                $current_post_id = get_the_ID();
+            } elseif (in_the_loop() && get_the_ID()) {
+                $current_post_id = get_the_ID();
+            }
+            
+            $has_image = false;
+            if($settings['source_type'] == 's_img' && !empty($settings['image']['id'])) {
+                $has_image = true;
+            } elseif($settings['source_type'] == 'f_img') {
+                if($current_post_id && has_post_thumbnail($current_post_id)) {
+                    $has_image = true;
+                } elseif(function_exists('is_product') && is_product()) {
+                    $post_future_image = northway()->get_theme_opt('post_future_image', '');
+                    if (!empty($post_future_image) && is_array($post_future_image) && !empty($post_future_image['background-image'])) {
+                        $has_image = true;
+                    }
+                }
+            }
         ?>
-        <div class="pxl-service--icon">
-            <?php echo wp_kses_post($icon_thumbnail); ?>
-        </div>
-    <?php endif; ?>
-<?php endif; ?>
-<?php if($settings['img_effect'] == 'pxl-image-border') : ?>
-    <div class="pxl-divide--group">
-        <div class="pxl-item--divider pxl-divider--top"></div>
-        <div class="pxl-item--divider pxl-divider--right"></div>
-        <div class="pxl-item--divider pxl-divider--bottom"></div>
-        <div class="pxl-divider--box"><div class="pxl-item--divider pxl-divider--left1"></div></div>
-        <div class="pxl-item--divider pxl-divider--left2"></div>
+        <?php if($has_image) : 
+            $image_size = !empty($settings['img_size']) ? $settings['img_size'] : 'full';
+            $img_id = null;
+            
+            if($settings['source_type'] == 's_img' && !empty($settings['image']['id'])) {
+                $img_id = $settings['image']['id'];
+            } elseif($settings['source_type'] == 'f_img') {
+                if (function_exists('is_product') && is_product() && $current_post_id) {
+                    $post_future_image = northway()->get_theme_opt('post_future_image', '');
+                    if (!empty($post_future_image) && is_array($post_future_image)) {
+                        if (!empty($post_future_image['background-image'])) {
+                            $bg_image_url = $post_future_image['background-image'];
+                            $bg_image_id = attachment_url_to_postid($bg_image_url);
+                            if ($bg_image_id) {
+                                $img_id = $bg_image_id;
+                            }
+                        }
+                    }
+                }
+                
+                if (empty($img_id) && $current_post_id && has_post_thumbnail($current_post_id)) {
+                    $img_id = get_post_thumbnail_id($current_post_id);
+                }
+            }
+            $img  = pxl_get_image_by_size( array(
+                'attach_id'  => $img_id,
+                'thumb_size' => $image_size,
+                'class' => 'no-lazyload'
+            ) );
+            $thumbnail    = $img['thumbnail'];
+            $thumbnail_url    = $img['url']; ?>
+
+            <?php switch ($settings['image_type']) {
+                case 'bg': ?>
+                    <div class="pxl-item--bg bg-image" style="background-image: url(<?php echo esc_url($thumbnail_url); ?>);">
+                        <?php if(!empty($settings['overlay_color'])) : ?>
+                            <div class="pxl-overlay-color"></div>
+                        <?php endif; ?>
+                    </div>
+                    <?php break;
+
+                default: ?>
+                    <div class="pxl-item--image" data-parallax-value="<?php echo esc_attr($settings['parallax_value']); ?>">
+                        <?php if ( ! empty( $settings['image_link']['url'] ) ) { ?><a <?php pxl_print_html($widget->get_render_attribute_string( 'image_link' )); ?>><?php } ?>
+                            <?php if ( ! empty( $img_id ) ) { echo wp_kses_post($thumbnail); } ?>
+                        <?php if ( ! empty( $settings['image_link']['url'] ) ) { ?></a><?php } ?>
+                        <?php if(!empty($settings['overlay_color'])) : ?>
+                            <div class="pxl-overlay-color"></div>
+                        <?php endif; ?>
+                    </div>
+                    <?php break;
+            } ?>
+        <?php endif; ?>
     </div>
-<?php endif; ?>
-</div>
 </div>
