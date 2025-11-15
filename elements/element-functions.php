3c647cf649a2e8eef0d43daaf14a6c8037323bbc
hongducdev <contact.hongduc@gmail.com>
2025-11-15 15:02:41 +0700

    feat(post-carousel): introduce new layout and style enhancements for post carousel
    - Add a second layout option for the post carousel with updated styles for improved visual appeal.
    - Implement new CSS and SCSS styles, including transitions, hover effects, and responsive design elements.
    - Enhance maintainability by organizing styles and refactoring existing code for consistency across components.

--- elements/element-functions.php
+++ elements/element-functions.php
@@ -408,6 +408,10 @@ function northway_get_carousel_layout_options($post_type_name){
                 'label' => esc_html__( 'Layout 1', 'northway' ),
                 'image' => get_template_directory_uri() . '/elements/widgets/img-layout/pxl_post_carousel/post-layout1.jpg'
             ],
+            'post-2' => [
+                'label' => esc_html__( 'Layout 2', 'northway' ),
+                'image' => get_template_directory_uri() . '/elements/widgets/img-layout/pxl_post_carousel/post-layout2.jpg'
+            ],
         ];
         break;
     }
